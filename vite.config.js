5a8cd850b684fe827d128f2da8b2c1eb7add937e
thanhbinh1907 <binh50135@gmail.com>
2025-11-11 21:33:58 +0700

    ádas

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  root: '.', // gốc dự án
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        home: resolve(__dirname, 'src/pages/home.html'),
+        signup: resolve(__dirname, 'src/pages/signup.html'),
+        download: resolve(__dirname, 'src/pages/download.html'),
+        signin: resolve(__dirname, 'src/pages/signin.html'),
+        features: resolve(__dirname, 'src/pages/features.html'),
+        help: resolve(__dirname, 'src/pages/help-center.html'),
+        resources: resolve(__dirname, 'src/pages/resources.html'),
+        board: resolve(__dirname, 'src/pages/Board.html'),
+        blog1: resolve(__dirname, 'src/pages/blog-article.html'),
+        blog2: resolve(__dirname, 'src/pages/blog-tips.html'),
+        blog3: resolve(__dirname, 'src/pages/blog-teamwork.html'),
+      },
+    },
+  },
+})
